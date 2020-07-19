54d2625f02030d9413a8c7463148fbda9dd6b9b2
Yerson Fernando Guzman Cano <yerson.guzman@ceiba.com.co>
2020-07-19 18:54:09 -0500

    service registro

---
+++ Frontend/src/app/service/registro.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Registro } from '../estacionamiento/modelo/regsitro';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistroService {
+
+  constructor(private http: HttpClient) { }
+
+  crear(registro: Registro): Observable<Registro> {
+    return this.http.post<Registro>('http:localhost:8080U/registro', registro);
+  }
+
+  actualizar(id: number): Observable<Registro> {
+    return this.http.put<Registro>('http://localhost:8080/registro/' + id, null);
+  }
+
+  listar(): Observable<Registro[]> {
+    return this.http.get<Registro[]>('http://localhost:8080/registro');
+  }
+
+  listarPorPlaca(placa: string ): Observable<Registro[]> {
+    return this.http.get<Registro[]>('http://localhost:8080/registro/' + placa);
+  }
+
+  eliminarRegistro() {
+    return this.http.delete<>('');
+  }
+}
