1f35cc9fb7dfd2fccc9735c6a16792d9abb0f5c0
Yerson Fernando Guzman Cano <yerson.guzman@ceiba.com.co>
2020-07-19 20:19:59 -0500

    front

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { EstacionamientoComponent } from './estacionamiento/estacionamiento.component';
 import { FormsModule } from '@angular/forms';
 import { RegistroService } from './service/registro.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RegistroService } from './service/registro.service';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [RegistroService],
   bootstrap: [AppComponent]
