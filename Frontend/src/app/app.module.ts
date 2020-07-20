367b28e5073f506546b45be0c0c0a12cf2ecbb03
Yerson Fernando Guzman Cano <yerson.guzman@ceiba.com.co>
2020-07-19 19:57:47 -0500

    error de enrutamiento

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { EstacionamientoComponent } from './estacionamiento/estacionamiento.component';
+import { FormsModule } from '@angular/forms';
+import { RegistroService } from './service/registro.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,10 @@ import { EstacionamientoComponent } from './estacionamiento/estacionamiento.comp
     EstacionamientoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [RegistroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
